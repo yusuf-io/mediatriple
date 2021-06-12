98db0b92431f5d29fbaebcecc1ad62401d216664
yusuf-io <yusufismailoglu@yahoo.com>
2021-06-12 12:46:49 +0300

    Add vuetify, vueI18n

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import vueI18n from "vue-i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
+  vueI18n,
   render: (h) => h(App),
 }).$mount("#app");
